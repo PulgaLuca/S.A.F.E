12a484e7712591b1befb06243b3d20f047aa9b7b
LucaPulga <68555926+LucaPulga@users.noreply.github.com>
2024-10-25 18:32:41 +0200

    First demo (partially functioning)

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
 # app.py
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate  # Importa Flask-Migrate
+from flask_migrate import Migrate
 from config import SafeConfig, SafeMiddleConfig
 from models import db
 from blueprints.weather import weather_bp
 from blueprints.events import events_bp
+import pandas as pd
 
 def create_app():
     app = Flask(__name__)
@@ -15,15 +16,23 @@ def create_app():
     app.config.from_object(SafeMiddleConfig)
     
     db.init_app(app)
-    migrate = Migrate(app, db)  # Inizializza Flask-Migrate
+    migrate = Migrate(app, db)
 
     # Registra i blueprint
     app.register_blueprint(weather_bp)
     app.register_blueprint(events_bp)
-    
+
     @app.route('/')
     def index():
-        return render_template('index.html')
+        # Leggi i CSV
+        weather_data = pd.read_csv(r'C:\Users\Luca\Desktop\weather_data.csv')
+        event_data = pd.read_csv(r'C:\Users\Luca\Desktop\event_data_with_location.csv')
+        
+        # Converti i DataFrame in dizionari
+        weather_data_dict = weather_data.to_dict(orient='records')
+        event_data_dict = event_data.to_dict(orient='records')
+
+        return render_template('index.html', weather_data=weather_data_dict, event_data=event_data_dict)
     
     return app
 
